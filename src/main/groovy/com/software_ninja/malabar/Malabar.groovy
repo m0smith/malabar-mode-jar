3bba565e28e177d384a3ddbf90af7f0ac894a069
Matthew O. Smith <matt@m0smith.com>
2014-12-01 09:31:54 -0700

    Run as a stand-alone

--- src/main/groovy/com/software_ninja/malabar/Malabar.groovy
+++ src/main/groovy/com/software_ninja/malabar/Malabar.groovy
@@ -1,15 +1,24 @@
 
 
-@Grapes([
-//@Grab(group='org.apache.maven', module='maven-core', version='3.0.5'),
-  @Grab(group='org.apache.maven', module='maven-compat', version='3.0.5'),
-  @Grab(group='com.jcabi', module='jcabi-aether', version='0.10.1')
-])
 
 package com.software_ninja.malabar;
 
-def start () {
-  new com.software_ninja.malabar.http.MalabarServer().start();
+import groovy.grape.Grape
+
+public static void main(String[] args) {
+  start();
 }
+
+def start (){
+    ClassLoader classLoader = new groovy.lang.GroovyClassLoader()
+    Map[] grapez = [
+		    [group : 'org.apache.maven', module : 'maven-compat', version :'3.0.5'],
+		    [group : 'com.jcabi', module : 'jcabi-aether', version : '0.10.1']];
+    Grape.grab(classLoader: classLoader, grapez)
+    //println "Class: " + classLoader.loadClass('org.ccil.cowan.tagsoup.jaxp.SAXParserImpl')
+    classLoader.loadClass('com.software_ninja.malabar.http.MalabarServer').newInstance().start();
+    
+}
+
 
 //http://localhost:4428/pi/repo=c:/users/Smith/.m2/repository&pom=c:/Users/Smith/projects/malabar-mode-jar/pom.xml
