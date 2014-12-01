7f3dba3ca540b91c095ebd0934eb81c1a9ba472a
Matthew O. Smith <matt@m0smith.com>
2014-12-01 07:27:50 -0700

    add the ability to install to local repo

--- src/main/groovy/com/software_ninja/malabar/Malabar.groovy
+++ src/main/groovy/com/software_ninja/malabar/Malabar.groovy
@@ -6,8 +6,10 @@
   @Grab(group='com.jcabi', module='jcabi-aether', version='0.10.1')
 ])
 
-class XX{}
+package com.software_ninja.malabar;
 
-new com.software_ninja.malabar.http.MalabarServer().start();
+def start () {
+  new com.software_ninja.malabar.http.MalabarServer().start();
+}
 
 //http://localhost:4428/pi/repo=c:/users/Smith/.m2/repository&pom=c:/Users/Smith/projects/malabar-mode-jar/pom.xml
