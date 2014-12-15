a84f5a291747caff758c399b36a9fa0be1d381b5
Matthew O. Smith <matt@m0smith.com>
2014-12-15 07:03:14 -0700

    fixed brokn test

--- src/test/groovy/com/software_ninja/malabar/TestResourceCache.groovy
+++ src/test/groovy/com/software_ninja/malabar/TestResourceCache.groovy
@@ -15,16 +15,18 @@ import org.junit.Before;
 import static org.junit.Assert.*
 
 import com.software_ninja.malabar.ResourceCache;
-     
+import com.software_ninja.malabar.MalabarUtil;
+       
 /**
  *  A resource cache stores a "map" of the contents of jar files to the path to the file.
  **/
    
 class TestResourceCacheImpl {
+    private String jar = MalabarUtil.expandFile("~/.m2/repository/xalan/xalan/2.7.1/xalan-2.7.1.jar"); 
   @Test
   public void testCache() {
     def rc = new ResourceCache();
-    rc.submit("c:/users/lpmsmith/.m2/repository/xalan/xalan/2.7.1/xalan-2.7.1.jar");
+    rc.submit(jar);
     String name = "org/apache/xpath/XPathVisitor.class";
     assertEquals (name, rc.find(name,10).first().getKey());
     name = "org.apache.xpath.XPathVisitor";
@@ -39,7 +41,7 @@ class TestResourceCacheImpl {
  @Test
   public void testFindClass() {
     def rc = new ResourceCache();
-    rc.submit("c:/users/lpmsmith/.m2/repository/xalan/xalan/2.7.1/xalan-2.7.1.jar");
+    rc.submit(jar);
     def name = /XP.*V.*[^.]*$/;
     assertEquals ("org.apache.xpath.jaxp.JAXPVariableStack", rc.findClass(name,10).first().getKey());
   }
