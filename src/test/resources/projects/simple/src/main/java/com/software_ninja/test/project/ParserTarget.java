5d4214aa7fcc2966a2139d396b652f761a678573
Matthew O. Smith <matt@m0smith.com>
2015-01-01 10:53:04 -0700

    gh-21 The java parser returns the class

---
+++ src/test/resources/projects/simple/src/main/java/com/software_ninja/test/project/ParserTarget.java
@@ -0,0 +1,31 @@
+ package com.software_ninja.test.project;
+
+/**
+ * Hello world!
+ *
+ */ 
+
+class PreParserTarget{}
+ 
+interface PreInterface {}
+
+public class ParserTarget 
+{ 
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    
+    public boolean getTrue() {
+	return true;
+    }
+
+    PreInterface pi = new PreInterface(){};
+
+    class Inner {}
+
+    static class StaticInner{}
+
+}
+
